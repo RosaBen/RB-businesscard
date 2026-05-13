6569ecad12b6d27e148c4ec621cc5e338f4b4a58
RosaBen <benchabane.rosa@outlook.com>
2026-05-14 00:46:32 +0200

    business card done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
 
+import Header from "./components/Header"
+import Footer from "./components/Footer"
+import Main from "./components/Main"
+
+
 function App() {
 
 
   return (
     <>
-
+    <div className="container">
+      <Header/>
+      <Main/>
+    </div>
+        <div className="container">
+      <Footer/>
+    </div>
+    
     </>
   )
 }
